d0d72a8a188105e2ca01841abe5a625706692a55
parthiv-2006 <parthiv.paul5545@gmail.com>
2025-06-29 17:47:15 -0400

    added a regular pen and magic pen, regular pen is black ink, magic pen is rainbow ink

--- main.js
+++ main.js
@@ -1,11 +1,31 @@
 const gridContainer = document.querySelector('.grid-container')
 
 const resetButton = document.querySelector('#resetBtn')
+const eraserButton = document.querySelector('#eraserBtn')
+const penButton = document.querySelector('#penBtn')
+const magicPenButton = document.querySelector('#magicPenBtn')
+
+let penColor = 'black'
 
 resetButton.addEventListener('click', () => {
     gridContainer.textContent = ''
     createGrid(16)
 })
+
+magicPenButton.addEventListener('click', () => {
+    penColor = 'magic'
+})
+
+penButton.addEventListener('click', () => {penColor = 'black'})
+
+eraserButton.addEventListener('click', () => {penColor = 'white'})
+
+function rainbowColor () {
+    const r = Math.floor(Math.random() * 256)
+    const g = Math.floor(Math.random() * 256)
+    const b = Math.floor(Math.random() * 256)
+    return `rgb(${r}, ${g}, ${b})`
+}
 
 function createGrid (gridSize) {
     const containerWidth = gridContainer.clientWidth
@@ -18,8 +38,13 @@ function createGrid (gridSize) {
         squareDiv.style.width = `${squareSize}px`
         squareDiv.style.height = `${squareSize}px`
 
+
         squareDiv.addEventListener('mouseover', () => {
-            squareDiv.style.backgroundColor = 'black'
+            if (penColor === 'magic') {
+                squareDiv.style.backgroundColor = rainbowColor()
+            }
+            else 
+             {squareDiv.style.backgroundColor = penColor}
         })
 
         gridContainer.appendChild(squareDiv)
