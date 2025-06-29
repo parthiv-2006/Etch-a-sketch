b163396f4ffffa1ee2f4c79426e392bff118b393
parthiv-2006 <parthiv.paul5545@gmail.com>
2025-06-29 17:12:43 -0400

    added functionality for reset button, clears grid on click

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 const gridContainer = document.querySelector('.grid-container')
 
+const resetButton = document.querySelector('#resetBtn')
+
+resetButton.addEventListener('click', () => {
+    gridContainer.textContent = ''
+})
+
 function createGrid (gridSize) {
     const containerWidth = gridContainer.clientWidth
     
